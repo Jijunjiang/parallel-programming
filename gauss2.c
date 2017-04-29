2cd5380fd2c4c9a60fcd5ccc772e04508ac443c8
Jijunjiang <jiangjijun1994@gmail.com>
2017-04-29 11:05:21 -0500

    resolve by jijun

---
+++ gauss2.c
@@ -0,0 +1,305 @@
+/* Gaussian elimination without pivoting.
+ */
+/*
+                                      discription
+ this file is a shared memory parallel algorithm (for Gaussian elimination)
+ using explicit parallel programming constructs (pthread)
+
+In this c program, since in the Gaussian elimination algorithm, each time we eliminate one
+colum, except the raw below the norm row do not have dependence with each other, could do parallel 
+computing.
+we use interleaving schedule to do memory share for different processors with chunk size of 10
+
+added elements:
+CHUNK_SIZE 10 // the chunk size for interleving
+int global_n // global number of norm, used to control the termination of parallel computing
+void *gausspart(void *n)// part of the guassian could be parelleled, each time for different value of norm,
+                        //the left part of the matrix do not have any dependence, could do parallel computing
+
+*/
+/* ****** ADD YOUR CODE AT THE END OF THIS FILE. ******
+ * You need not submit the provided code.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <sys/time.h>
+#include <limits.h>
+///////////////////////////////////////////////////////////////////
+#include <pthread.h>
+#define CHUNK_SIZE 10 // the chunk size for interleving
+///////////////////////////////////////////////////////////////////
+
+/*#include <ulocks.h>
+#include <task.h>
+*/
+
+char *ID;
+
+/* Program Parameters */
+#define MAXN 10000  /* Max value of N */
+int N;  /* Matrix size */
+int procs;  /* Number of processors to use */
+
+/* Matrices and vectors */
+volatile float A[MAXN][MAXN], B[MAXN], X[MAXN];
+/* A * X = B, solve for X */
+
+/* junk */
+#define randm() 4|2[uid]&3
+/////////////////////////////////////////////////////////////////
+int global_n; // global number of norm, used to control the termination of parallel computing
+pthread_mutex_t global_n_lock, sum_lock;
+
+/* part of the guassian could be parelleled */
+void *gausspart(void *n);
+
+///////////////////////////////////////////////////////////////////
+
+/* Prototype */
+
+void gauss();  /* The function you will provide.
+		* It is this routine that is timed.
+		* It is called only on the parent.
+		*/
+
+/* returns a seed for srand based on the time */
+unsigned int time_seed() {
+  struct timeval t;
+  struct timezone tzdummy;
+
+  gettimeofday(&t, &tzdummy);
+  return (unsigned int)(t.tv_usec);
+}
+
+/* Set the program parameters from the command-line arguments */
+void parameters(int argc, char **argv) {
+  int submit = 0;  /* = 1 if submission parameters should be used */
+  int seed = 0;  /* Random seed */
+  char uid[L_cuserid + 2]; /*User name */
+
+  /* Read command-line arguments */
+  //  if (argc != 3) {
+  if ( argc == 1 && !strcmp(argv[1], "submit") ) {
+    /* Use submission parameters */
+    submit = 1;
+    N = 4;
+    procs = 2;
+    printf("\nSubmission run for \"%s\".\n", cuserid(uid));
+      /*uid = ID;*/
+    strcpy(uid,ID);
+    srand(randm());
+  }
+  else {
+    if (argc == 3) {
+      seed = atoi(argv[3]);
+      srand(seed);
+      printf("Random seed = %i\n", seed);
+    }
+    else {
+      printf("Usage: %s <matrix_dimension> <num_procs> [random seed]\n",
+	     argv[0]);
+      printf("       %s submit\n", argv[0]);
+      exit(0);
+    }
+  }
+    //  }
+  /* Interpret command-line args */
+  if (!submit) {
+    N = atoi(argv[1]);
+    if (N < 1 || N > MAXN) {
+      printf("N = %i is out of range.\n", N);
+      exit(0);
+    }
+    procs = atoi(argv[2]);
+    if (procs < 1) {
+      printf("Warning: Invalid number of processors = %i.  Using 1.\n", procs);
+      procs = 1;
+    }
+  }
+
+  /* Print parameters */
+  printf("\nMatrix dimension N = %i.\n", N);
+  printf("Number of processors = %i.\n", procs);
+}
+
+/* Initialize A and B (and X to 0.0s) */
+void initialize_inputs() {
+  int row, col;
+
+  printf("\nInitializing...\n");
+  for (col = 0; col < N; col++) {
+    for (row = 0; row < N; row++) {
+      A[row][col] = (float)rand() / 32768.0;
+    }
+    B[col] = (float)rand() / 32768.0;
+    X[col] = 0.0;
+  }
+
+}
+
+/* Print input matrices */
+void print_inputs() {
+  int row, col;
+
+  if (N < 10) {
+    printf("\nA =\n\t");
+    for (row = 0; row < N; row++) {
+      for (col = 0; col < N; col++) {
+	printf("%5.2f%s", A[row][col], (col < N-1) ? ", " : ";\n\t");
+      }
+    }
+    printf("\nB = [");
+    for (col = 0; col < N; col++) {
+      printf("%5.2f%s", B[col], (col < N-1) ? "; " : "]\n");
+    }
+  }
+}
+
+void print_X() {
+  int row;
+
+  if (N < 10) {
+    printf("\nX = [");
+    for (row = 0; row < N; row++) {
+      printf("%5.2f%s", X[row], (row < N-1) ? "; " : "]\n");
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  /* Timing variables */
+  struct timeval etstart, etstop;  /* Elapsed times using gettimeofday() */
+  struct timezone tzdummy;
+  clock_t etstart2, etstop2;  /* Elapsed times using times() */
+  unsigned long long usecstart, usecstop;
+  struct tms cputstart, cputstop;  /* CPU times for my processes */
+
+  ID = argv[argc-1];
+  argc--;
+
+  /* Process program parameters */
+  parameters(argc, argv);
+
+  /* Initialize A and B */
+  initialize_inputs();
+
+  /* Print input matrices */
+  print_inputs();
+
+  /* Start Clock */
+  printf("\nStarting clock.\n");
+  gettimeofday(&etstart, &tzdummy);
+  etstart2 = times(&cputstart);
+  /////////////////////////////////
+  pthread_mutex_init(&global_n_lock,NULL);
+  /////////////////////////////////////
+  /* Gaussian Elimination */
+  gauss();
+
+  /* Stop Clock */
+  gettimeofday(&etstop, &tzdummy);
+  etstop2 = times(&cputstop);
+  printf("Stopped clock.\n");
+  usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
+  usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
+
+  /* Display output */
+  print_X();
+
+  /* Display timing results */
+  printf("\nElapsed time = %g ms.\n",
+	 (float)(usecstop - usecstart)/(float)1000);
+  pthread_exit(NULL);
+
+
+}
+
+/* ------------------ Above Was Provided --------------------- */
+
+/****** You will replace this routine with your own parallel version *******/
+/* Provided global variables are MAXN, N, procs, A[][], B[], and X[],
+ * defined in the beginning of this code.  X[] is initialized to zeros.
+ */
+void gauss() {
+  int norm, row, col;  /* Normalization row, and zeroing
+			* element row and col */
+  
+  pthread_t threads[procs];
+  pthread_mutex_init(&global_n_lock,NULL);
+  global_n = 0;
+
+  /* Gaussian elimination */
+  for (norm = 0; norm < N - 1; norm++) {
+    global_n = norm;
+    for (int i=0; i < procs; i++) {
+      // since each time for different value of norm,
+      // the left part of the matrix do not have any dependence, could be paralleled
+      pthread_create(&threads[i],NULL,&gausspart,(void*)(norm));
+    }
+
+    for (int i=0; i < procs; i++) {
+      pthread_join( threads[i], NULL);
+    }
+  }
+
+  pthread_mutex_destroy(&global_n_lock);
+  
+  /* (Diagonal elements are not normalized to 1.  This is treated in back
+   * substitution.)
+   */
+
+  /* Back substitution */
+  for (row = N - 1; row >= 0; row--) {
+    X[row] = B[row];
+    for (col = N-1; col > row; col--) {
+      X[row] -= A[row][col] * X[col];
+    }
+    X[row] /= A[row][row];
+  }
+}
+
+
+
+// part of the guassian could be parelleled, each time for different value of norm,
+// the left part of the matrix do not have any dependence, could be paralleled
+void *gausspart(void *n) {
+
+  //raw_s is the index of raw the calculation should be start
+  //raw_end is the index of raw the calculation should be terminated
+  int row, col;
+  float multiplier;
+  long norm;
+  int row_s;
+  int row_end;
+
+  norm = (long)n;
+  row_s = 0;
+  while (row_s < N) {
+    pthread_mutex_lock(&global_n_lock);
+    row_s = global_n + 1;
+    // implement of interleving to do parallel computing
+    global_n += CHUNK_SIZE;
+    row_end = row_s + CHUNK_SIZE;
+    // to prevent outofbound
+    if (row_end >= N) {
+      row_end = N;
+    }
+    pthread_mutex_unlock(&global_n_lock);
+    for (row = row_s; row < row_end; row++) {
+      multiplier = A[row][norm] / A[norm][norm];
+      for (col = norm; col < N; col++) {
+    A[row][col] -= A[norm][col] * multiplier;
+      }
+      B[row] -= B[norm] * multiplier;
+    }
+    row_s = row;
+  }
+}
+
+
+
